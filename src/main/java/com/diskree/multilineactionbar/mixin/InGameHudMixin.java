ca67a75b6cd38366011615c12f9f0deea463539a
diskree <diskreee@gmail.com>
2024-04-06 16:32:32 +0300

    1.19.1+

--- src/main/java/com/diskree/multilineactionbar/mixin/InGameHudMixin.java
+++ src/main/java/com/diskree/multilineactionbar/mixin/InGameHudMixin.java
@@ -2,10 +2,9 @@ package com.diskree.multilineactionbar.mixin;
 
 import net.minecraft.client.font.MultilineText;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -31,16 +30,16 @@ public abstract class InGameHudMixin {
         overlayMessageText = MultilineText.create(getTextRenderer(), message, scaledWidth - 50);
     }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTextWithShadow(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;III)I", ordinal = 0))
-    private int renderRedirect(DrawContext context, TextRenderer textRenderer, @NotNull Text text, int x, int y, int color) {
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;drawWithShadow(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I", ordinal = 0))
+    private int renderRedirect(TextRenderer textRenderer, MatrixStack matrices, Text text, float x, float y, int color) {
         int linesCount = overlayMessageText.count();
         if (linesCount > 1) {
             if (linesCount % 2 == 1) {
                 linesCount--;
             }
-            y -= (linesCount * textRenderer.fontHeight) / 2;
+            y -= (float) (linesCount * textRenderer.fontHeight) / 2;
         }
-        overlayMessageText.drawCenterWithShadow(context, 0, y, textRenderer.fontHeight, color);
+        overlayMessageText.drawCenterWithShadow(matrices, 0, (int) y, textRenderer.fontHeight, color);
         return 0;
     }
 }
