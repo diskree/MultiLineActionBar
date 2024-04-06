039eac1eb0f42ae673f51fe4b2b67d4c429da44c
diskree <diskreee@gmail.com>
2024-04-06 17:22:39 +0300

    1.16+

--- src/main/java/com/diskree/multilineactionbar/mixin/InGameHudMixin.java
+++ src/main/java/com/diskree/multilineactionbar/mixin/InGameHudMixin.java
@@ -1,9 +1,10 @@
 package com.diskree.multilineactionbar.mixin;
 
-import net.minecraft.class_5489;
+import com.google.common.collect.Lists;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.StringRenderable;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,11 +14,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.List;
+
 @Mixin(InGameHud.class)
 public abstract class InGameHudMixin {
 
     @Unique
-    private class_5489 overlayMessageText;
+    private final List<StringRenderable> overlayMessageTextLines = Lists.newArrayList();
 
     @Shadow
     private int scaledWidth;
@@ -27,19 +30,23 @@ public abstract class InGameHudMixin {
 
     @Inject(method = "setOverlayMessage", at = @At(value = "HEAD"))
     private void setOverlayMessageInject(Text message, boolean tinted, CallbackInfo ci) {
-        overlayMessageText = class_5489.method_30890(getFontRenderer(), message, scaledWidth - 50);
+        overlayMessageTextLines.clear();
+        overlayMessageTextLines.addAll(getFontRenderer().wrapLines(message, scaledWidth - 50));
     }
 
-    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/Text;FFI)I", ordinal = 0))
-    private int renderRedirect(TextRenderer textRenderer, MatrixStack matrices, Text text, float x, float y, int color) {
-        int linesCount = overlayMessageText.method_30887();
+    @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/text/StringRenderable;FFI)I", ordinal = 0))
+    private int renderRedirect(TextRenderer textRenderer, MatrixStack matrices, StringRenderable text, float x, float y, int color) {
+        int linesCount = overlayMessageTextLines.size();
         if (linesCount > 1) {
             if (linesCount % 2 == 1) {
                 linesCount--;
             }
             y -= (float) (linesCount * textRenderer.fontHeight) / 2;
         }
-        overlayMessageText.method_30889(matrices, 0, (int) y, textRenderer.fontHeight, color);
+        for (StringRenderable stringRenderable : this.overlayMessageTextLines) {
+            textRenderer.draw(matrices, stringRenderable, -textRenderer.getWidth(stringRenderable) / 2f, y, color);
+            y += textRenderer.fontHeight;
+        }
         return 0;
     }
 }
